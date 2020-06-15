55dea2614b0399d51eaff4204acc27bc70c25008
Adal <adal_vb@outlook.com>
2020-06-15 16:41:34 +0000

    nuevas carpetas

---
+++ APIs/src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+/*const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json([]);
+});
+
+module.exports = router;*/
+
+module.exports = function (app) {
+    app.get('/', (req, res) => {
+        res.json([]);
+    });
+}
