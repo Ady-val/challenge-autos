53c80ac842528c2cdf5f0a5e336e2ead9b945ecc
Adal <adal_vb@outlook.com>
2020-11-21 20:45:07 +0000

    api para obtener lista de carros

--- APIs/src/api/app.js
+++ APIs/src/api/app.js
@@ -29,7 +29,7 @@ app.get('/get_rol', (req, res) => {
             } else {
                 res.status(404).json({
                     status: 'error',
-                    message: 'no existen generos disponibles'
+                    message: 'no existen datos disponibles'
                 });
             }
         } else {
@@ -39,4 +39,71 @@ app.get('/get_rol', (req, res) => {
             });
         }
     })
+})
+
+app.get('/catalog/get_data/:user/:id_rol', (req, res) => {
+    const user = req.params.user
+    const id_rol = req.params.id_rol
+
+    if (id_rol === 1) {
+        var sql = 'SELECT * FROM cars';
+
+        mysqlConnection.query(sql, (err, rows) => {
+
+            if (!err) {
+                if (rows.length > 0) {
+                    var catalog = rows;
+                    var package = {
+                        status: 'success',
+                        message: 'generos',
+                        genders: catalog
+                    }
+
+                    res.status(200).send(package);
+
+                } else {
+                    res.status(404).json({
+                        status: 'error',
+                        message: 'no existen datos disponibles'
+                    });
+                }
+            } else {
+                res.status(500).json({
+                    status: 'error',
+                    message: '500'
+                });
+            }
+        })
+
+    } else {
+
+        var sql = 'SELECT * FROM cars WHERE id_user = ?';
+
+        mysqlConnection.query(sql, [user], (err, rows) => {
+
+            if (!err) {
+                if (rows.length > 0) {
+                    var catalog = rows;
+                    var package = {
+                        status: 'success',
+                        message: 'generos',
+                        genders: catalog
+                    }
+
+                    res.status(200).send(package);
+
+                } else {
+                    res.status(404).json({
+                        status: 'error',
+                        message: 'no existen datos disponibles'
+                    });
+                }
+            } else {
+                res.status(500).json({
+                    status: 'error',
+                    message: '500'
+                });
+            }
+        })
+    }
 })
