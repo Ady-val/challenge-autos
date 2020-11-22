e55dbba8bffe4f4f2eb5041bc9151400c61f62cc
Adal <adal_vb@outlook.com>
2020-11-22 16:52:15 +0000

    nuevas apis para agregar carro, actualizar carro y borrar carro

--- APIs/src/api/app.js
+++ APIs/src/api/app.js
@@ -51,7 +51,7 @@ app.get('/catalog/get_data/:user/', (req, res) => {
             const id_rol = rows[0].id_rol
 
             if (id_rol === '1') {
-                var sql = 'SELECT * FROM cars';
+                var sql = 'SELECT * FROM cars WHERE is_active = 1';
         
                 mysqlConnection.query(sql, (err, rows) => {
         
@@ -82,7 +82,7 @@ app.get('/catalog/get_data/:user/', (req, res) => {
         
             } else {
         
-                var sql = 'SELECT * FROM cars WHERE id_user = ?';
+                var sql = 'SELECT * FROM cars WHERE id_user = ? AND is_active = 1';
         
                 mysqlConnection.query(sql, [user], (err, rows) => {
         
@@ -121,7 +121,7 @@ app.get('/catalog/get_data/:user/', (req, res) => {
     })
 })
 
-app.put('catalog/add_car', (req, res) => {
+app.post('/catalog/add_car/', (req, res) => {
     const plates = req.body.plates
     const brand = req.body.brand
     const color = req.body.color
@@ -130,17 +130,73 @@ app.put('catalog/add_car', (req, res) => {
     const lon = req.body.lon
     const id_user = req.body.id_user
 
-    var sql = 'INSERT INTO cars (plates, brand, color, year, lat, lon, id_user, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())'
+    var sql = 'SELECT TOP 1 * FROM cars WHERE plates = ?'
 
-    mysqlConnection.query(sql, [plates, brand, color, year, lat, lon, id_user], (err, rows) => {
+    mysqlConnection.query(sql, [plates], (err, rows) => {
         if (!err) {
-            var package = {
-                status: 'success',
-                message: 'guardado con exito',
+            if (rows.length !== 0) {
+                var sql = 'INSERT INTO cars (plates, brand, color, year, lat, lon, id_user, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())'
+
+                mysqlConnection.query(sql, [plates, brand, color, year, lat, lon, id_user], (err, rows) => {
+                    if (!err) {
+                        var package = {
+                            status: 'success',
+                            message: 'guardado con exito',
+                        }
+
+                        res.status(200).send(package);
+
+                    } else {
+                        res.status(500).json({
+                            status: 'error',
+                            message: '500'
+                        });
+                    }
+                })
+            } else {
+                res.status(500).json({
+                    status: 'error',
+                    message: 'ya existe ese carro con esas placas'
+                });
             }
+        }
+    })
+})
 
-            res.status(200).send(package);
+app.post('/catalog/update_car/', (req, res) => {
+    const last_plates = req.body.last_plates
+    const plates = req.body.plates
+    const brand = req.body.brand
+    const color = req.body.color
+    const year = req.body.year
+    const lat = req.body.lat
+    const lon = req.body.lon
 
+    var sql = 'SELECT id FROM cars WHERE plates = ?'
+
+    mysqlConnection.query(sql, [last_plates], (err, rows) => {
+        if (!err) {
+            const id = rows[0].id
+
+            sql = 'UPDATE cars SET plates = ' + plates + ', brand = ' + brand + ', color = ' + color + ', year = ' + year + ', lat = ' + lat + ', lon = ' + lon + ' WHERE id = ' + id
+
+            mysqlConnection.query(sql, (err, rows) => {
+                if (!err) {
+
+                    var package = {
+                        status: 'success',
+                        message: 'guardado con exito',
+                    }
+
+                    res.status(200).send(package);
+
+                } else {
+                    res.status(500).json({
+                        status: 'error',
+                        message: '500'
+                    });
+                }
+            })
         } else {
             res.status(500).json({
                 status: 'error',
@@ -148,4 +204,8 @@ app.put('catalog/add_car', (req, res) => {
             });
         }
     })
+})
+
+app.post('/catalog/delete_car/', (req, res) => {
+    const plates = req.body.plates
 })
